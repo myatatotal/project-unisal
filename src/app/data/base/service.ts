05386abcb08ff0ef3dbd92deca7c8f40175ab385
Fabio Ferreira <fabiorobertoferreira@gmail.com>
2022-06-11 09:06:31 -0300

    novo

--- src/app/data/base/service.ts
+++ src/app/data/base/service.ts
@@ -16,26 +16,26 @@ export class ApiService {
 
   getProdutos() {
     return this.http
-      .get<any>('http://localhost:3000/produtos')
+      .get<any>('http://ec2-3-145-100-173.us-east-2.compute.amazonaws.com/products')
       //     .get<any>('https://803f-3-145-100-173.ngrok.io/products')
       .pipe(map((res: any) => res));
   }
 
   getClientes() {
     return this.http
-      .get<any>('http://localhost:3000/clientes')
+      .get<any>('http://ec2-3-145-100-173.us-east-2.compute.amazonaws.com/clients')
       .pipe(map((res: any) => res));
   }
 
   getCategorias() {
     return this.http
-      .get<any>('http://localhost:3000/categorias')
+      .get<any>('http://ec2-3-145-100-173.us-east-2.compute.amazonaws.com/categories')
       .pipe(map((res: any) => res));
   }
 
   delete(id: string) {
     return this.http
-      .delete<any>(`http://localhost:3000/produtos/${id}`)
+      .delete<any>(`http://ec2-3-145-100-173.us-east-2.compute.amazonaws.com/${id}`)
       .pipe(map((res: any) => res));
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   version: '1.0.0',
   serverUrl: 'http://localhost:3000',
-  server: 'https://803f-3-145-100-173.ngrok.io',
+  server: 'http://ec2-3-145-100-173.us-east-2.compute.amazonaws.com/',
   defaultLanguage: 'pt-BR',
   supportedLanguages: [
     'pt-BR'
